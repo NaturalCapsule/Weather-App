f88961cb57ed4aa7400271750841a3a3216588b9
HamzaNouman <hamzanouman2009@gmail.com>
2025-07-12 00:13:37 +0300

    modified:   get_weather_info.py

--- get_weather_info.py
+++ get_weather_info.py
@@ -1,12 +1,12 @@
 import geocoder
+import httpx
 
 g = geocoder.ip('me')
-
 
 
 city = g.city
 city = str(city)
 if ' ' in city:
     city = city.replace(' ', '+')
-link = f'https://wttr.in/{city}?format=1'
-print(link)
+link = httpx.get(f'https://wttr.in/{city}?format=j1')
+print(link.text)
