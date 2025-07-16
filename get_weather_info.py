3d1e7555fe65519b1c345ade96d3c44edc0469cb
HamzaNouman <hamzanouman2009@gmail.com>
2025-07-17 00:32:57 +0300

    Display Weather

--- get_weather_info.py
+++ get_weather_info.py
@@ -1,56 +1,64 @@
 import geocoder
 import httpx
+from PyQt6.QtGui import QPixmap
+from io import BytesIO
+
+class WeatherInfo:
+    def __init__(self):
+        self.g = None
+        self.city = None
+        self.link = None
+        self.data = None
+        self.icon = None
+
+    def get_json_weather_data(self):
+        self.g = geocoder.ip('me')
+        self.city = str(self.g.city).replace(' ', '+')
+    
+        self.link = httpx.get(f'https://wttr.in/{self.city}?format=j1')
+        self.data = self.link.json()
+        weather_code = self.data['current_condition'][0].get('weatherCode')
+        self.icon = f"http://cdn.weatherapi.com/weather/128x128/day/{weather_code}.png"
+
+        return self.data, self.icon
+
+    def get_c_weather(self):
+
+        data = self.get_json_weather_data()
+
+        temp_c = data['current_condition'][0]['temp_C']
+        print(f'Temperature in Celsius: {temp_c}°C')
+        return temp_c
 
 
-def get_json_weather_data():
-    g = geocoder.ip('me')
-    city = g.city
+    def get_sunrise(self):
+        data = self.get_json_weather_data()
 
-    if not city:
-        print("City not found!")
-        return
-
-    city = city.replace(' ', '+')
-    link = httpx.get(f'https://wttr.in/{city}?format=j1')
-
-    data = link.json()
-    return data
-
-def get_c_weather():
-
-    data = get_json_weather_data()
-
-    temp_c = data['current_condition'][0]['temp_C']
-    return temp_c
+        sunrise = data['weather'][0]['astronomy'][0]['sunrise']
+        return sunrise
 
 
-def get_sunrise():
-    data = get_json_weather_data()
+    def get_sunset(self):
+        data = self.get_json_weather_data()
 
-    sunrise = data['weather'][0]['astronomy'][0]['sunrise']
-    return sunrise
+        sunset = data['weather'][0]['astronomy'][0]['sunset']
+        return sunset
 
+    def get_moonset(self):
+        data = self.get_json_weather_data()
 
-def get_sunset():
-    data = get_json_weather_data()
+        moonset = data['weather'][0]['astronomy'][0]['moonset']
+        return moonset
 
-    sunset = data['weather'][0]['astronomy'][0]['sunset']
-    return sunset
+    def get_moonrise(self):
+        data = self.get_json_weather_data()
 
-def get_moonset():
-    data = get_json_weather_data()
+        moonrise = data['weather'][0]['astronomy'][0]['moonrise']
+        return moonrise
 
-    moonset = data['weather'][0]['astronomy'][0]['moonset']
-    return moonset
+    def get_desc(self):
+        data = self.get_json_weather_data()
 
-def get_moonrise():
-    data = get_json_weather_data()
+        desc = data['weather'][0]['hourly'][2]['weatherDesc'][0]['value']
+        return desc
 
-    moonrise = data['weather'][0]['astronomy'][0]['moonrise']
-    return moonrise
-
-def get_desc():
-    data = get_json_weather_data()
-
-    desc = data['weather'][0]['hourly'][2]['weatherDesc'][0]['value']
-    return desc
--- readme.md
+++ readme.md
@@ -1,12 +1,25 @@
-Install Modules: `pip install -r requirements.txt`
-
-1-install geocoder:
+# 1-install geocoder:
 ```bash
 pip install geocoder
 ```
 
-2-install httpx:
+# 2-install httpx:
 ```bash
 
 pip install httpx
+```
+
+# 3-install PyQt6:
+```bash
+pip install PyQt6 PyQt6-tools
+```
+
+# 4-install Fluent_window_widgets:
+```bash
+pip install pyqt6-fluent-widgets
+```
+
+# Or You Can Install Modules: 
+```bash 
+pip install -r requirements.txt
 ```
